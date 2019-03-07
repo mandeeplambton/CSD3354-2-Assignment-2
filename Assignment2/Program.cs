b448faa68330d23d9a3333e8aa433872fc02c90b
visualstudio <visualstudio@230-PC43>
2019-03-07 16:48:14 -0500

    lab11

--- Assignment2/Program.cs
+++ Assignment2/Program.cs
@@ -1,34 +1,34 @@
 ﻿using System;
-namespace DelegatesAndEvents
-{ 
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public class Program
+
+// name:Mandeep kaur(730387)
+//name: Amandeep kaur(730499)
+namespace DelegateAndEvents
 {
-    public static void Main()
+    class program
     {
-        DelegateExercises delegateExercises = new DelegateExercises();
-        delegateExercises.Method3();
-        Console.ReadLine();
+        static void Main(string[] args)
+        {
+            DelegateExercises a = new DelegateExercises();
+            a.Method3();
+            Console.ReadLine();
+        }
     }
-}
-
-public delegate void MyDelegate();
-
-public class DelegateExercises
-{
-    void Method1()
+    public delegate void MyDelegate();
+    public class DelegateExercises
     {
-        System.Console.WriteLine("Method1");
+        void Method1()
+        {
+            System.Console.WriteLine("Method1");
+        }
+        public void Method3()
+        {
 
-
+            System.Console.WriteLine(MyDelegate.ToString());
+        }
     }
-
-    public void Method3()
-    {
-        MyDelegate myDelegate = new MyDelegate(Method1);
-        myDelegate();
-        System.Console.WriteLine(myDelegate.ToString());
-    }
-}
-
 }
